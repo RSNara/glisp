c087993edc97a88cf33757c758436d0757cb70a5
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-08-01 20:32:25 -0400

    fix(root-env): Fix '=' function

--- src/root-env.js
+++ src/root-env.js
@@ -1,21 +1,22 @@
 import * as I from 'immutable';
 import * as M from 'mathjs';
+import * as R from 'ramda';
 
-export default {
-  [Symbol.for('+')]: (...args) => args.reduce((a, b) => M.add(a, b)),
-  [Symbol.for('-')]: (...args) => args.reduce((a, b) => M.subtract(a, b)),
-  [Symbol.for('*')]: (...args) => args.reduce((a, b) => M.multiply(a, b)),
-  [Symbol.for('/')]: (...args) => args.reduce((a, b) => M.divide(a, b)),
-  [Symbol.for('=')]: (...args) => args.reduce((a, b) => I.is(a, b)),
-  [Symbol.for('!')]: (x) => !x,
-  [Symbol.for('<')]: (...args) => conjunct((a, b) => M.smaller(a, b), args),
-  [Symbol.for('>')]: (...args) => conjunct((a, b) => M.larger(a, b), args),
-};
-
-function conjunct(fn, args) {
+const conjunct = R.curry((fn, args) => {
   const [first, ...rest] = args;
   return rest.reduce(
     ([result, old], current) => [result && fn(old, current), current],
     [true, first]
   )[0];
-}
+});
+
+export default {
+  [Symbol.for('+')]: (...args) => args.reduce((x, y) => M.add(x, y)),
+  [Symbol.for('-')]: (...args) => args.reduce((x, y) => M.subtract(x, y)),
+  [Symbol.for('*')]: (...args) => args.reduce((x, y) => M.multiply(x, y)),
+  [Symbol.for('/')]: (...args) => args.reduce((x, y) => M.divide(x, y)),
+  [Symbol.for('!')]: (x) => !x,
+  [Symbol.for('=')]: (...args) => conjunct((x, y) => I.is(x, y), args),
+  [Symbol.for('<')]: (...args) => conjunct((x, y) => M.smaller(x, y), args),
+  [Symbol.for('>')]: (...args) => conjunct((x, y) => M.larger(x, y), args),
+};

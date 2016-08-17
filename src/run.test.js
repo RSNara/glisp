1d936d7f114d2a0d10486443daeeecfacb885614
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-08-16 21:23:21 -0400

    Update tests to reflect examples

--- src/run.test.js
+++ src/run.test.js
@@ -5,17 +5,17 @@ import run from './run';
 test('should allow setting properties on javascript objects', (assert) => {
   const result = run(`
     (let [object (.toJS {})]
-        (aset object "my-key" 10))
+      (aset object "name" "Ramanpreet Nara")
+      (aset object "age" 20))
   `);
 
-  assert.deepEqual({ 'my-key': M.bignumber(10) }, result );
+  assert.deepEqual({ name: 'Ramanpreet Nara', age: M.bignumber(20) }, result );
 });
 
 test('should allow getting properties from javascript objects', (assert) => {
   const result = run(`
-    (let [object (.toJS {"my-key" 10})]
-        (aget object "my-key"))
+    (aget (.toJS {"age" 21}) "age")
   `);
 
-  assert.deepEqual(result, M.bignumber(10));
+  assert.deepEqual(result, M.bignumber(21));
 });
